7f76672284207e1bfeb7a80e7d60338f3cbff39e
Jakub Šťastný <60524926+jakub-stastny@users.noreply.github.com>
2021-05-25 05:24:41 -0500

    Create .profile.zsh

---
+++ .profile.zsh
@@ -0,0 +1,10 @@
+load ~/.zsh/environments/helpers.zsh && save-function-list
+load ~/.zsh/environments/basic.zsh
+
+start-emacs-session
+rename-first-tab
+
+# Custom functions & aliases.
+# ...
+        
+report-custom-functions
